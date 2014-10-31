816a55027c8a34fb93bfd607f1e1ff53c147bc48
lytze <liyutze.seikiri@gmail.com>
2014-11-01 03:15:09 +0800

    * functions.R; + demo.R
    * functions.R: finished the main functionalities, the analyze.cpi()
    function will give back a list of functions: short() for short text
    summary, log() for showing details and report() for generating a
    picture report;
    + demo.R

--- functions.R
+++ functions.R
@@ -1,103 +1,152 @@
 # Functions required for sectorring analyses
-# Li Yutze Oct 2014
+# Version 0.3 Li Yutze Oct 31 2014
 
-
-# Read the colony PEV image (CPI) into R
-import.cpi <- function(name, col.path = c(1.0, 1.0, 1.0)) {
-    # name: character element of length 1, identicating the path of the picture
-    # col: numeric vector of length 3, given the grabbing color
-    
-    if (!is.character(name)) {
-        stop('String of file location is expected, but not a string is inputed');
-    }
-    else {
-        if (length(col.path) != 3) {
-            stop('Color path is expected, but vector of length other than 3 is inputed');
-        } 
-        if (sum(col.path > 1.0 | col.path < 0.0) != 0) {
-            stop('Color path is expected, but number out of expected range is inputed');
-        }
-        if (sum(col.path == 1.0) < 1) {
-            stop('Saturated color path is expected, but not saturated color is inputed');
-        }
-    }
-    # Error cases:
-    ### input file name not a valid character vector
-    ### input color path not of valid length, range, or not saturated
-    
+analyze.cpi <- function(name, col.path = c(1.0, 1.0, 1.0), lay = c(0.3, 0.6, 0.9), div = 90) {
     require(jpeg);
-    load <- readJPEG(name);
-    cpi <- list(
-        identity = 'CPI',
-        call = match.call(),
-        raw = (load[, , 1] * col.path[1] + load[, , 2] * col.path[2] + load[, , 3] * col.path[3]) / 
-            sqrt(sum(col.path))
-    );
-    # Dot product, to grab the color conponent required
+    require(reshape);
+    require(stringr);
+    require(plotrix);
     
-    return(cpi);
-}
-
-# Get the background threshold and output the binary evaluation
-threshold.cpi <- function(cpi) {
-    # cpi: colony PEV image processing list
+    call <- match.call(); # record the function call, for reporting
+    filen <- str_extract(name, '[^/]*$');
+    filen <- gsub('.[^.]*$', '', filen);
     
-    current.mean <- mean(cpi$raw); 
-    escape <- 0;
-    counter <- 10;
+    img <- readJPEG(name);
+    img = (img[, , 1] * col.path[1] + img[, , 2] * col.path[2] + img[, , 3] * col.path[3]) / 
+        sqrt(sum(col.path));
+    tall <- melt(img, varnames = c('y', 'x')); 
+    tall$y <- -tall$y # cast the matrix data into tall list
+    
+    ras <- as.raster(img); # make the raw image a raster, for future ploting
+    den <- density(tall$value, n = 200); # record the density of color values, for future ploting
+    
+    # calculate the threshold for background color
+    current.mean <- mean(tall$value); # set current mean value
+    escape <- 0; # loop escape mark
+    counter <- 10; # count the loop number, escape the loop when loop over 10 times
     while (!escape & counter) {
         last.mean <- current.mean;
-        current.mean <- mean(cpi$raw[cpi$raw <= 2 * last.mean]);
+        current.mean <- mean(tall$value[tall$value <= 2 * last.mean]);
         # narrow down the sample range to [0, 2 * last.mean]
         counter <- counter - 1;
-        if (abs(last.mean - current.mean) <= 0.01) {
+        if (abs(last.mean - current.mean) <= 0.005) {
             escape <- 1;
         }
     }
-    # Iterative find the mean of background value. If not converge after 10
-    # cycles, escape from the loops
+    threshold <- current.mean * 2;
+    deter <- tall$value > threshold;
     
-    cpi$threshold <- current.mean * 2;
-    cpi$binary.map <- cpi$raw >= cpi$threshold;
+    # denoise
+    # scan the tall list, for each entry check the 11 * 11 square area
+    # if over 30% points are in the list, mark all points in the list to be not noise
+    # if less than 30% points are in the list, delet all
+    # scaning processes should skip all entries marked to be not noise
     
-    # compress the binary map and create a copy
-    cby <- nrow(cpi$binary.map) %/% 100; 
-    compressed <- 
-        cpi$binary.map[seq(from = 1, to = nrow(cpi$binary.map), by = cby),
-                       seq(from = 1, to = ncol(cpi$binary.map), by = cby)];
+    # evaluate the circular ragion
+    cen.x <- mean(tall$x[deter]);
+    cen.y <- mean(tall$y[deter]);
+    radius <- sqrt(sum(deter) / pi);
     
-    cpi$compressed <- compressed;
-    cpi$comp.by <- cby;
-    return(cpi);
+    # get overall intensity
+    intens <- mean(tall$value[deter]);
+    
+    # get hierarchy verctors
+    rads <- seq(from = 0, to = 2 * pi, length = div + 1);
+    rads <- rads[1:div];
+    cx <- round(sapply(lay, function(x) {cen.x + cos(rads) * x * radius}), 0);
+    cy <- round(sapply(lay, function(x) {cen.y + sin(rads) * x * radius}), 0);
+    vec <- matrix(ncol = div, nrow = length(lay));
+    for (i in 1:length(lay)) {
+        vec[i, ] <- mapply(function(x, y) {
+            mean(c(img[-y + -3:3, x], img[-y, x + -3:3]));
+        }, cx[, i], cy[, i]);
+        vec[i, ] <- (vec[i, ] - mean(vec[i, ]));
+    }
+    
+    # get horizontal variation complexity
+    vec.stp <- vec[, 1:div] - vec[, c(div, 1:(div-1))];
+    hori <- mean(apply(vec.stp, 2, sd));
+    
+    # get vertical variation complexity
+    m <- as.matrix(dist(vec));
+    vert <- mean(m[1:(length(lay) - 1), 2:length(lay)]);
+    
+    # remove unessesary variables
+    rm(current.mean, last.mean, escape, counter, 
+       tall, deter, m, i, rads, vec.sum);
+    
+    # generate short report
+    short <- function(pr = F) {
+        if (pr) {
+            cat('Overall Intensity: ', intens, '\n', sep = '');
+            cat('Horizontal Variation Complexity: ', hori, '\n', sep = '');
+            cat('Vertical Variation Complexity: ', vert, '\n', sep = '');
+        }
+        invisible(list(intensity = intens, horizontal = hori, vertical = vert));
+    }
+    
+    # give processing log
+    log <- function(pr = F) {
+        if (pr) {
+            cat('Identifier: ', filen, '\n', sep = '');
+            cat('Threshold: ', threshold, '\n', sep = '');
+            cat('Evaluating Colony Shape:\n\tcenter: (x, y) = (',
+                cen.x,' ,', cen.y, ')\tradius: ', radius, '\n', sep = '');
+        }
+        invisible(list(call = call, threshold = threshold,
+                       xcenter = cen.x, ycenter = cen.y, radius = radius));
+    }
+    
+    # build report
+    report <- function() {
+        ori.par <- par(no.readonly = T);
+        opt <- paste(paste(lay * 100, collapse = '-'), '-', div, sep = '');
+        png(paste('report-', filen, '-', opt, '.png', sep = ''), 
+            width = 600, height = 900);
+        # plot the density and threshold
+        par(fig = c(0.1, 0.9, 0.85, 1), mar = c(0, 0, 0, 0));
+        plot(c(1, 7), c(1, 7), type = 'n', ann = F, axes = F);
+        text(4, 6, paste('REPORT #', filen), cex = 2);
+        text(3, 4:2, c('Overall Intensity:',
+                       'Horizontal Variation Complexity:',
+                       'Vertical Variation Complexity:'), cex = 1);
+        text(5.3, 4:2, round(c(intens, hori, vert), 5), cex = 1);
+        # plot the intensity distribution and the threshold
+        par(fig = c(0.1, 0.9, 0.62, 0.86), mar = c(5, 4, 4, 4) + 0.1, new = T); 
+        plot(den, ann= F, axes = F, col = 'blue4');
+        abline(v = threshold, col = 'red2', lty = 2);
+        axis(side = 2, cex = 0.5, tck = -0.02, cex = 0.6);
+        axis(side = 1, cex = 0.5, tck = -0.02, cex = 0.6);
+        title(main = 'Intensity Distribution',
+              xlab = 'Intensity', ylab = 'Density', cex = 0.6);
+        legend('bottom', legend = c('density', 'threshold'), 
+               lty = c(1, 2), col = c('blue4', 'red2'), cex = 0.8);
+        # show the colony shape
+        shift <- 0.24 / ncol(ras) * nrow(ras);
+        par(fig = c(0.14, 0.86, 0.4 - shift, 0.4 + shift), mar = c(0, 0, 0, 0), new = T);
+        plot(c(1, ncol(ras)), c(-1, -nrow(ras)), type = 'n', ann = F, axes = F);
+        rasterImage(ras, 1, -1, ncol(ras), -nrow(ras));
+        points(cen.x, cen.y, col = 'red2', pch = 4);
+        draw.circle(cen.x, cen.y, radius, border = 'red2', lty = 1);
+        for (i in 1:length(lay)) {
+            #draw.circle(cen.x, cen.y, radius * i, border = 'orange3', lty = 2);
+            points(cx[, i], cy[, i], col = 'orange3', pch = '.')
+        }
+        text(c(cen.x, cen.x), c(cen.y, cen.y + radius), c('center', 'bondary'),
+             col = 'red', cex = 0.8, pos = 3, offset = 0.4);
+        text(rep(cen.x, length(lay)), cen.y + radius * lay, paste(lay * 100, '%'),
+                 col = 'orange', cex = 0.8, pos = 3, offset = 0.4);
+        # plot the sample vecters
+        par(fig = c(0.1, 0.9, 0.03, 0.2), mar = c(2, 2, 3, 2), new = T);
+        image(t(vec), ann = F, axes = F);
+        axis(side = 2, at = seq(from = 0, to = 1, length = length(lay)),
+             labels = paste(lay * 100, '%'), cex.axis = 0.8, las = 2, tck = 0, 
+             col.axis = 'orange3',line = -0.5, lwd = 0);
+        dev.off();
+        par(ori.par);
+    }
+    
+    return(list(short = short,
+                log = log,
+                report = report));
 }
-
-# Get the center and the radius of the fitted circle
-circle.cpi <- function(cpi) {
-    # cpi: colony PEV image processing list
-    
-    require(reshape);
-    stick.map <- melt(cpi$compressed);
-    stick.map <- stick.map[stick.map[, 3] == T, 1:2];
-    
-    center.row <- mean(stick.map[, 1]);
-    center.col <- mean(stick.map[, 2]);
-    # center location is the center of weight of the binary map
-    
-    area <- nrow(stick.map) # = sum(cpi$binary.map), count all TURE dots
-    radius <- sqrt(area / pi);
-    
-    cpi$stick <- stick.map;
-    cpi$cen.row <- center.row * cpi$comp.by;
-    cpi$cen.col <- center.col * cpi$comp.by;
-    cpi$radius <- radius * cpi$comp.by;
-    
-    cpi
-}
-
-
-
-cpi <- import.cpi('sample_figure/3.jpg', c(0, 0, 1));
-cpi <- threshold.cpi(cpi);
-cpi <- circle.cpi(cpi);
-
-png('report.png', )
